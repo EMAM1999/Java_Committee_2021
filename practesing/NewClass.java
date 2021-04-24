4f32c2bb834e42ac48524c3732f85605b9600850
EMAM <65142314+EMAM1999@users.noreply.github.com>
2021-04-24 23:17:09 +0200

    Add files via upload

---
+++ practesing/NewClass.java
@@ -0,0 +1,26 @@
+/*
+ To change this license header, choose License Headers in Project Properties.
+ To change this template file, choose Tools | Templates
+ and open the template in the editor.
+ */
+package practesing;
+
+/**
+
+ @author EMAM
+ */
+public class NewClass {
+//        s = 3
+//        c = 2
+//        x = 2
+
+
+          public static void main(String[] args) {
+//                    int x = BankAccount.SAVING_ACCOUNT;
+//                    BankAccount.SAVING_ACCOUNT = BankAccount.CITIZEN_ACCOUNT;
+//                    BankAccount.CITIZEN_ACCOUNT = x;
+
+                    BankAccount account1 = new BankAccount(1 , "aya" , 1000 , BankAccount.SAVING_ACCOUNT , true);
+          }
+
+}
